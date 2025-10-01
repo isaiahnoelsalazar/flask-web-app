da01096f3cf4314b35d85424a45f082454682f7b
isaia <isaiahnoelsalazar474@gmail.com>
2025-10-01 23:38:33 +0800

    update: removed YouTube downloader link

--- api/index.py
+++ api/index.py
@@ -1,42 +1,10 @@
-from flask import Flask, render_template, request, redirect
-from pytubefix import YouTube
-from io import BytesIO
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/download', methods=['POST'])
-def download_video():
-    video_url = request.form.get('video_url')
-
-    if not video_url:
-        return 'Error: Please provide a YouTube URL.'
-
-    try:
-        yt = YouTube(video_url, use_po_token=True)
-
-        video_stream = yt.streams.filter(progressive=True, file_extension='mp4', resolution='360p').first()
-
-        if not video_stream:
-            video_stream = yt.streams.get_lowest_resolution()
-
-        '''if not video_stream:
-            return 'Error: Could not find a suitable progressive MP4 stream.'
-
-        buffer = BytesIO()
-        video_stream.stream_to_buffer(buffer)
-        buffer.seek(0)
-
-        return send_file(buffer, as_attachment=True, download_name=f"{yt.title}.mp4", mimetype='video/mp4')'''
-
-        direct_link = video_stream.url
-
-        return direct_link
-    except Exception as e:
-        return f"An error occurred: {e}"
+    return 'Hello'
 
 @app.route('/about')
 def about():
