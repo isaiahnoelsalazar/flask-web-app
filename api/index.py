8803bfd7b641f18b75b97350086cec75dd7c6a79
isaia <isaiahnoelsalazar474@gmail.com>
2025-10-02 15:34:50 +0800

    test: testing new functions

--- api/index.py
+++ api/index.py
@@ -9,3 +9,28 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route("/mssql")
+def pymssql():
+    try:
+        server = 'sql.bsite.net\\MSSQL2016'
+        database = 'saiasamazingaspsite_SampleDB'
+        username = 'saiasamazingaspsite_SampleDB'
+        password = 'DBSamplePW'
+
+        connection = pymssql.connect(server, username, password, database)
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'")
+        rows = cursor.fetchall()
+
+        allstr = ""
+
+        for row in rows:
+            allstr += str(row)[2:-3] + "\n"
+
+        connection.close()
+
+        return allstr
+    except Exception as e:
+        return str(e)
